6ca8c9eed3690ea16d52abcbf09db5cf076a8aeb
besailazi <126988974+besailazi@users.noreply.github.com>
2024-05-08 11:05:29 +0200

    import firebase method

--- src/js/app.js
+++ src/js/app.js
@@ -2,8 +2,30 @@ import {firebaseConfig} from "./firebaseConfig"
 
 import { initalizedApp } from "firebase/app"
 
-import { getAuth } from "firebase/auth"
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged } from "firebase/auth"
 
 initalizedApp(firebaseConfig)
 
 const authService = getAuth();
+
+const emailInput = document.querySelector('.email');
+const passwordInput = document.querySelector('.password');
+const signInButton = document.querySelector('.sign-in-button');
+const signOutButton = document.querySelector('.sign-out-button');
+const signUpButton = document.querySelector('.sign-up-button');
+const errorMessage = document.querySelector('.error-message');
+
+const frontPage = document.querySelector('.front-page');
+const secretSection = document.querySelector('.secret-section');
+
+signUpButton.addEventListener('click', ()=>{
+
+})
+
+signInButton.addEventListener('click', ()=>{
+
+})
+
+signOutButton.addEventListener('click', ()=>{
+
+})
