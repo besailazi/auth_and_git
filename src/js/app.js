73170aabf5873285d2bfa022bf3da6bf26ddcc98
besailazi <126988974+besailazi@users.noreply.github.com>
2024-05-08 11:18:12 +0200

    Add eventlisteners and render function

--- src/js/app.js
+++ src/js/app.js
@@ -18,14 +18,56 @@ const errorMessage = document.querySelector('.error-message');
 const frontPage = document.querySelector('.front-page');
 const secretSection = document.querySelector('.secret-section');
 
-signUpButton.addEventListener('click', ()=>{
+signUpButton.addEventListener('click', (event)=>{
+	event.preventDefault();
+	createUserWithEmailAndPassword(authService, emailInput.value, passwordInput.value)
+	.then(()=>{
+		checkAuthStateAndRender();
+	})
+	.catch((error)=>{
+      errorMessage.style.display = "block";
+		errorMessage.textContent = error.message
+	})
 
 })
 
-signInButton.addEventListener('click', ()=>{
-
+signInButton.addEventListener('click', (event)=>{
+   event.preventDefault();
+	signInWithEmailAndPassword(authService, emailInput.value, passwordInput.value)
+	.then(()=>{
+      checkAuthStateAndRender();
+	})
+	.catch((error)=>{
+      errorMessage.style.display = "block";
+		errorMessage.textContent = error.message
+	})
 })
 
-signOutButton.addEventListener('click', ()=>{
-
+signOutButton.addEventListener('click', (event)=>{
+   event.preventDefault();
+	signOut(authService)
+	.then(()=>{
+		checkAuthStateAndRender();
+		console.log('signed out');
+	})
+	.catch((error)=>{
+		console.log(error.message);
+	})
 })
+
+function checkAuthStateAndRender(){
+  onAuthStateChanged(authService, (user)=>{
+    if(user) {
+		errorMessage.textContent = "";
+		frontPage.style.display = "none";
+		secretSection.style.display = "block";
+		signOutButton.style.display = "block";
+	 }else {
+		frontPage.style.display = "block";
+		secretSection.style.display = "none";
+		signOutButton.style.display = "none";
+	 }
+  })
+}
+
+checkAuthStateAndRender()
